d133875307b867c7be5e0d22dc0d3bb64877a7e6
SrS27a <sawyer.r.stephens@gmail.com>
2020-12-12 15:48:07 -0800

    fixed a bug that would cause userinfo to error out if there was no roles on the user

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -275,8 +275,8 @@ class Info(commands.Cog, name='Information Commands'):
         embed.add_field(name='Color', value=f"```{member.color}```")
         embed.add_field(name='Bot', value=f"```{member.bot}```")
         embed.add_field(name='Key Permissions', value=f"```{array}```", inline=False)
-        embed.add_field(name=f'Roles [{amount}]',
-                        value=" ".join([role.mention for role in roles if role.name != "@everyone"]), inline=False)
+        if roles:
+            embed.add_field(name=f'Roles [{amount}]', value=" ".join([role.mention for role in roles if role.name != "@everyone"]), inline=False)
         embed.set_thumbnail(url=member.avatar_url)
         await ctx.send(embed=embed)
 
