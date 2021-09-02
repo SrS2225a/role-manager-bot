29aed675893c96e024659eb8b2ffbc50076d71df
SrS2225a <sawyer.r.stephens@gmail.com>
2021-09-02 11:44:25 -0700

    fix for new invite channel feature thing

--- bot/cogs/leaderboard.py
+++ bot/cogs/leaderboard.py
@@ -890,6 +890,7 @@ class Leaderboard(commands.Cog, name='Leaderboards & Counters'):
     @commands.command(aliases=['invitecodes'], brief="inviteinfo @Vendron#2001")
     async def inviteinfo(self, ctx, *, member: discord.User = None):
         """Shows info about your invites, or someone else's"""
+        global channelMention
         cursor = await self.bot.db.acquire()
         guild = ctx.guild
         member = member or ctx.author
@@ -897,7 +898,10 @@ class Leaderboard(commands.Cog, name='Leaderboards & Counters'):
             "SELECT amount, amount2, amount3, invite, channel FROM invite WHERE guild = $1 and member = $2", guild.id, member.id)
         embed = discord.Embed(title=f"Invite Info For {member}", color=member.color)
         for invite in full:
-            embed.add_field(name=f"{invite[3]} - {guild.get_channel(invite[4]).mention}",
+            channel = guild.get_channel(invite[4])
+            if channel is not None:
+                channelMention = channel.mention
+            embed.add_field(name=f"{invite[3]} - {channelMention}",
                             value=f"**{invite[0]}** joins, **{invite[1]}** leaves, **{invite[2]}** fakes", inline=False)
 
         await ctx.send(embed=embed)
