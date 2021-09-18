49f57371fe622d1d7a7fdcde1cb8c6fc9b31e618
SrS2225a <sawyer.r.stephens@gmail.com>
2021-09-17 20:14:07 -0700

    added a way to update users on who gets bot blacklisted

--- bot/cogs/jishaku.py
+++ bot/cogs/jishaku.py
@@ -15,8 +15,11 @@ class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
             await cursor.execute("DELETE FROM blacklist WHERE member = $1", user.id)
             await ctx.send(f"Successfully unblocked {user} from Dionysus with the reason {reason}")
         else:
+            server = self.bot.get_guild(531247629649182750)
+            channel = server.get_channel(841455998542938143)
             await cursor.execute("INSERT INTO blacklist(member, message) VALUES($1, $2)", user.id, reason)
             await ctx.send(f"Successfully blocked {user} from Dionysus with the reason {reason}")
+            await channel.send(f"**{user}** was just blacklisted from using Dionysus for **{reason}**!")
 
 
 def setup(bot: commands.Bot):
