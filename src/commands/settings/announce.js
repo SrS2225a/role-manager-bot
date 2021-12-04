3e87e19ea0674843d1c7aa9590c669eec2cfcc24
SrS2225a <sawyer.r.stephens@gmail.com>
2021-12-03 18:43:53 -0800

    fif for announce setting

--- src/commands/settings/announce.js
+++ src/commands/settings/announce.js
@@ -13,19 +13,19 @@ module.exports = {
     async execute(message) {
         userPermissions(message, ["MANAGE_GUILD"]);
         const db = await pool.connect();
-        const result = await db.query("SELECT announce FROM settings WHERE announce = $1 and guild = $2", [message.options.getChannel('announce').id, message.guild.id])
+        const result = await db.query("SELECT announce FROM settings WHERE announce = $1 and guild = $2", [message.options.getChannel('channel').id, message.guild.id])
         const search = await db.query("SELECT announce FROM settings WHERE guild = $1", [message.guild.id])
         if (result.rowCount === 0) {
             if (search.rowCount === 0) {
-                await db.query("INSERT INTO settings(guild, announce) VALUES($1, $2)", [message.guild.id, message.options.getChannel('announce').id])
-                message.reply(`Set the announce to ${message.options.getChannel('announce').name}`)
+                await db.query("INSERT INTO settings(guild, announce) VALUES($1, $2)", [message.guild.id, message.options.getChannel('channel').id])
+                message.reply(`Set the announce to ${message.options.getChannel('channel').name}`)
             } else {
-                await db.query("UPDATE settings SET announce = $1 WHERE guild = $2", [message.options.getChannel('announce').id, message.guild.id])
-                message.reply(`Set the announce to ${message.options.getChannel('announce').name}`)
+                await db.query("UPDATE settings SET announce = $1 WHERE guild = $2", [message.options.getChannel('channel').id, message.guild.id])
+                message.reply(`Set the announce to ${message.options.getChannel('channel').name}`)
             }
         } else {
-            await db.query("UPDATE settings SET announce = $1 WHERE guild = $2", [message.options.getChannel('announce').id, message.guild.id])
-            message.reply(`Set the announce to ${message.options.getChannel('announce').name}`)
+            await db.query("UPDATE settings SET announce = $1 WHERE guild = $2", [message.options.getChannel('channel').id, message.guild.id])
+            message.reply(`Set the announce to ${message.options.getChannel('channel').name}`)
         }
         await db.release();
     }
